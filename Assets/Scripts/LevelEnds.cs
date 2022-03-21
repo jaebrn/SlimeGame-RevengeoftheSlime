473092dc02ed481c4b3fb0baa4fbc20da0fde61d
jaebrn <80350744+jaebrn@users.noreply.github.com>
2022-03-21 10:04:27 -0400

    End flag/Scene change
    next scene loads upon collision with flag at the end of the level. Only added to IntroLevel so far

--- Assets/Scripts/LevelEnds.cs
+++ Assets/Scripts/LevelEnds.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnds : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   // Array of all scenes in build; as new ones become added, their names must be added to this string array
+    string[] sceneArray = {"IntroLevel", "BouncePadLevel"};
+    // set this in the inspector so that the index matches the string
+    //e.g: sceneIndex 0 == Intro Level
+    public int sceneIndex; 
+    
+    
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        SceneManager.LoadScene(sceneArray[sceneIndex + 1]);
+        print("scene change");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
