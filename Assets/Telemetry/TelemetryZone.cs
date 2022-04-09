2a5f018832c50b7dc30e1f6ce83f7073033d3db8
kyikii <alexdicarlo60@gmail.com>
2022-04-08 23:51:16 -0400

    Telemetry sections RE-added
    sections are up and working though the same bug with having 2 telemetryloggers loaded occurs

---
+++ Assets/Telemetry/TelemetryZone.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelemetryZone : MonoBehaviour
+{
+    public string sectionName;
+
+    private void OnTriggerEnter2D(Collider2D Collider2D) {
+        if (Collider2D.TryGetComponent<PlayerControllerCurrent>(out var controller));
+            var logger = GameObject.FindObjectOfType<TelemetryLogger>();
+
+        //logger.ChangeSection(sectionName);
+            TelemetryLogger.GetLogger(this).ChangeSection(sectionName);
+    }
+
+}
